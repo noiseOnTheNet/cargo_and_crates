63f7ef80bf6cec94656f1bee93265bba0967c8e2
Marco Paolo Valerio Vezzoli <marco.p.v.vezzoli@gmail.com>
2021-12-28 19:06:29 +0100

    initial post info

---
+++ src/poly.rs
@@ -0,0 +1,7 @@
+pub mod poly {
+    struct Poly {
+    }
+}
+fn main(){
+    println!("hello poly world!");
+}
---
+++ tests/test_poly.rs
@@ -0,0 +1,10 @@
+mod poly;
+#[cfg(test)]
+use poly;
+
+mod test {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
